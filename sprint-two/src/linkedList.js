26d724de57a709742d1e5ad1c37797dc576254bd
Shane Sutro <shanesutro@gmail.com>
2021-08-24 22:00:49 -0600

    Complete linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,35 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    const newNode = new Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+    }
+    if (list.tail === null) {
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    const formerHead = list.head.value;
+    list.head = list.head.next;
+    return formerHead;
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
+    var node = list.head;
+    while (node !== null) {
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
 
   return list;
